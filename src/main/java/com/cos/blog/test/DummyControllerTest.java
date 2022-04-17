386a331d9065c4553842662a88c1ad852e161916
김정민 <jmkim9302@gmail.com>
2022-04-17 23:17:17 +0900

    put, get dummy user controller with paging

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -4,14 +4,34 @@ import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class DummyControllerTest {
 
     @Autowired  // 의존성 주입 (DI)
     private UserRepository userRepository;
+
+    @GetMapping("/dummy/users")
+    public List<User> list() {
+        return userRepository.findAll();
+    }
+
+    // 한 페이지당 2건에 데이터를 return
+    @GetMapping("/dummy/user")
+    public List<User> pageList(@PageableDefault(size = 2, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+        Page<User> pagingUsers = userRepository.findAll(pageable);
+        List<User> users = pagingUsers.getContent();
+
+        return users;
+    }
 
     @PostMapping("/dummy/join")
     public String join(User user) {
@@ -37,5 +57,26 @@ public class DummyControllerTest {
 //
 //        return "회원가입이 완료되었습니다.";
 //    }
+
+    // email, password
+    @Transactional
+    @PutMapping("/dummy/user/{id}")
+    public User updateUser(@PathVariable int id,
+                           @RequestBody User requestUser) {
+        System.out.println("id: " + id);
+        System.out.println("password: " + requestUser.getPassword());
+        System.out.println("email: " + requestUser.getEmail());
+
+        User user = userRepository.findById(id).orElseThrow(() -> {
+            return new IllegalArgumentException("수정에 실패하였습니다.");
+        });
+
+        user.setPassword(requestUser.getPassword());
+        user.setEmail(requestUser.getEmail());
+
+//        userRepository.save(user);
+
+        return null;
+    }
 
 }
